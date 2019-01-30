3eb0cadfb3c413cac1eb3e810cd52b3cdf67b1c9
mary ferrara <maryferrara@marys-iMac.local>
2019-01-29 22:00:42 -0500

    latest project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
+// initialize the configuration of map
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+  // use JS's built-in Navigator to get user's lat/lng coordinates
+  navigator.geolocation.getCurrentPosition(function(position) {
+    // create an object to store lat/lng data
+    var userLocation = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+       // create a new instance of a map
+    // configure map with options object
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: userLocation,
+      zoom: 10,
+      scrollwheel: false
+    });
+
+    // use Marker constructor to add a marker to map
+    var marker = new google.maps.Marker({
+      position: userLocation,
+      map: map,
+      title: 'User Location'
+    });
+
   });
 }
+
+initMap();
