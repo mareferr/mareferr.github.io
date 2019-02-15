c6976ea7ca9d614127e76911d5d29f33308c73dc
mary ferrara <maryferrara@marys-iMac.local>
2019-02-15 08:40:40 -0500

    yeep

--- js/main.js
+++ js/main.js
@@ -24,15 +24,15 @@
 
 $(document).ready(function ()  
 {  
-  var startTime="17:00:00"; // or 5:00
- var endTime ="21:00:00"  // or 9:00 
+  var startTime="05:00:00"; // or 5:00
+ var endTime ="21:00:00";  // or 9:00 
 var dt = new Date();
 var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 
-if(time >startTime || time < endTiime){
- $("#time").html("Open Now");
+if(time > startTime || time < endTime){
+ $("#time").html("<h2>Open Now</h2>");
 }else{
- $("#time").html("Closed Now");
+ $("#time").html("<h2>Closed Now</h2>");
 }
 });
 
@@ -41,29 +41,41 @@ if(time >startTime || time < endTiime){
  $('#reservationForm').on('submit', function (e) {
   // prevent the page from reloading
   e.preventDefault();
-  // grab user's comment from input field
-  var userInputName = $('#reservationName').val();
-    var userInputDay = $('#reservationDay').val();
 
-
-            $('#name').html(e.userInputName);
-              $('#day').html(e.userInputDay);
-    
-  // clear the user's comment from the input (for UX purposes)
-  $('#reservationName').val('');
-   $('#reservationDay').val('');
-  // create a section for reservations data in your db
+    // create a section for reservations data in your db
   var resReference = database.ref('reservationData');
+
+  // if either the name or day field are not empty push the values to the db
+if( $('#reservationName').val() != '' && $('#reservationDay').val() != '' ){
+
+
+
+
   // use the push method to push data to the reservation section
   resReference.push({
-    name: userInputName,
-    day: userInputDay
+  name: $('#reservationName').val(),
+        day: $('#reservationDay').val(),
+
+//likes: $('#likes').val()
+        
+      })
+  //reset input forms to empty
+      reservationForm.reset();
+
+      // If input fields were submitted empty send error message and don't output any html
+    } else {
+      alert('Please enter both your name and desired reservation date');
+       $("#reservationName").html('') && $("#reservationday").html('');
+    }
   });
-});
+
 
  //get reservation data to put on web page
 function getReservations() {
   database.ref('reservationData').on('value', function (results) {
+
+
+
     // Get all reservations stored in the results we received back from Firebase
     var allReservations = results.val();
     // Set an empty array where we can add all reservations we'll append to the DOM
@@ -71,28 +83,24 @@ function getReservations() {
     // iterate (loop) through all reservations coming from database call
     for (var item in allReservations) {
       // Create an object literal with the data we'll pass to Handlebars
+      console.log(allReservations[item]);
       var context = {
         name: allReservations[item].name,
         day: allReservations[item].day,
-        reservationId: ID
+        //likes: allReservations[item].likes
        };
       // Get the HTML from our Handlebars comment template
-      var source = $("#reservation-template").html();
+      var source = $("#reservations-template").html();
       // Compile our Handlebars template
       var template = Handlebars.compile(source);
-      // Pass the data for this comment (context) into the template
+      // Pass the data for this reservation (context) into the template
       var resListElement = template(context);
-      // push newly created element to array of comments
-      reservationData.push(resListElement);
+  // append each comment to the list of reservations in the DOM
+      $("#reservations").append(resListElement);
 
-    // remove all list items from DOM before appending list items
-    $('.reservationData').empty();
-    // append each comment to the list of comments in the DOM
-    for (var i in reservationData) {
-      $('.reservationData').append(reservationData[i])
-    }
-  };
+  }
 });
+}
 
  
 
@@ -100,20 +108,72 @@ function getReservations() {
 // When page loads, call getReservations function
 getReservations();
 
-$('.reservations').on('click', '.delete', function (e) {
-  // Get the ID for the reservation we want to delete
-  var id = $(e.target).parent().data('id')
 
-  // find reservation whose objectId is equal to the id we're searching with
-  var resReference = database.ref('reservationData/' + id)
+//$('#reservations').on('click', '.likes', function (e) {
 
-//remove reservation from web page
-            $(this).html('');
-          
+  // Get the ID from the parent of the like button we clicked on
+
+
+  // find comment whose objectId is equal to the id we're searching with
+  //var resReference = database.ref('reservationData/');
+
+  // Get number of likes from HTML
+  //var likes = $('#likes').html();
+
+  // Convert likes to a number and add a like
+  //likes = parseInt(likes, 10) + 1;
+
+  // Update likes property in database using Firebase's update() method.
+  //resReference.update({
+    //likes: likes
+ // });
+
+//});
+
+
+$('#reservations').on('click', '.delete', function (e) {
+  // Get the ID for the comment we want to update
+
+  e.preventDefault();
+ var resReference = database.ref('reservationData/').child;
+
+
+
+
+   resReference.remove(e);
+  
+}); 
+
+
+
+
+
+
+
+
+
+$('#reservations').on('click', '.edit', function (e) {
+  // Get the ID for the comment we want to update
+
+  e.preventDefault();
+
+
+  var resReference = database.ref('reservationData/');
+
+
+
   // Use remove method to remove the comment from the database
-  resReference.remove()
+  resReference.update();
+
 });
 
+//remove reservation from web page
+
+  // use the push method to push data to the reservation section
+ 
+          
+  // Use remove method to remove the reservation from the database
+ 
 
 
 
